20ce1805112f16bbc0fdd5aaf232298d6d12dfb2
Sebastian Roming <sebastian@webmonkey.io>
2024-03-28 10:30:00 +0100

    samesitenone

--- app/load/page.tsx
+++ app/load/page.tsx
@@ -1,7 +1,7 @@
 export default function Test() {
   return (
     <>
-      <iframe src="https://00213936-frame-to-be-loaded.preview.vercel-support.app?x-vercel-protection-bypass=vteQk13gOCIXj5Vpj2dwvmWsz71OnnPq&x-vercel-set-bypass-cookie=samesite" width="50%" height="50%"></iframe>
+      <iframe src="https://00213936-frame-to-be-loaded.preview.vercel-support.app?x-vercel-protection-bypass=vteQk13gOCIXj5Vpj2dwvmWsz71OnnPq&x-vercel-set-bypass-cookie=samesitenone" width="50%" height="50%"></iframe>
     </>
   )
 }
