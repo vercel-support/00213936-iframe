3512f9adef375a399c92788ca1b3586ea6c8b933
Sebastian Roming <sebastian@webmonkey.io>
2024-03-28 08:28:06 +0100

    initial

---
+++ app/load/page.tsx
@@ -0,0 +1,7 @@
+export default function Test() {
+  return (
+    <>
+      <iframe src="https://00213936-frame-to-be-loaded.preview.vercel-support.app" width="50%" height="50%"></iframe>
+    </>
+  )
+}
